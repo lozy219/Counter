e256533666300c4b78785d4802fe570b7336000b
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-09 21:17:33 +0800

    clean

--- static/js/main.js
+++ static/js/main.js
@@ -111,52 +111,9 @@ function updateRecords() {
 }
 
 function listRecord(i) {
-	console.log(record);
 	var listHtml = "<li class='comp' data-goto='people'>" +
 						"<div><h3>" + record[i].desc + "</h3></div>" +
 						"<aside><h4>" + record[i].count + "</h4></aside>" +
 					"</li>";
 	$('#record-list').append(listHtml);
 }
-
-
-
-
-
-
-
-// $('#goto-new-note').on('singletap', function() {
-// 	$.UIGoToArticle("#note-new");
-// });
-
-// $('#home-note-list').on('singletap', function() {
-// 	var id = $(this).data('id');
-// 	viewNote(id);
-// });
-
-// var notes = [
-// 	{
-// 		title : "SimpleNote Intro",
-// 		content : "Simple haha"
-	
-// 	}
-// ];
-
-// for (var i = 0; i < notes.length; i ++) {
-// 	listNode(i);
-// }
-
-// function listNode(id) {
-// 	$("home-note-list").append("<li class='nav' data-id='" + id + "'>" +
-// 							   "<h3>" + notes[id].title + "</h3>" + 
-// 							   "<h4>" + notes[id].content.substr(0, 30) + "...</h4>" +
-// 							   "</li>"
-// 							  );
-// }
-
-// function viewNote(id) {
-// 	$('#note-view').data('id', id);
-// 	$('#note-view .note-title').text(note[id].title);
-// 	$('#note-view .note-content').html(notes[id].content);
-// 	$.UIGoToArticle('#note-view');
-// }
