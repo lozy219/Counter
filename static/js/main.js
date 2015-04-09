411658ddce8f0e581805ca0da8e77e8534047831
Lei Mingyu <uygnim.iel@gmail.com>
2015-04-09 18:29:54 +0800

    fix reset bug

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,13 @@ var stats = {
 	totalTap : 0,
 }
 
+$('#stats-reset').on('singletap', function() {
+	stats = {
+		totalTap : 0,
+	};
+	updateStats();
+});
+
 $('#counter').on('tap', function() {
 	$('#counter-count').html($('#counter-count').html() - 1 + 2);
 	stats.totalTap ++;
@@ -11,14 +18,6 @@ $('#counter').on('tap', function() {
 
 $('#counter-reset').on('singletap', function() {
 	$('#counter-count').html(0);
-});
-
-$('#stats-reset').on('singletap', function() {
-	alert('a');
-	// stats = {
-	// 	totalTap : 0,
-	// };
-	// updateStats();
 });
 
 $(function() {
@@ -38,38 +37,38 @@ function updateStats() {
 
 
 
-$('#goto-new-note').on('singletap', function() {
-	$.UIGoToArticle("#note-new");
-});
+// $('#goto-new-note').on('singletap', function() {
+// 	$.UIGoToArticle("#note-new");
+// });
 
-$('#home-note-list').on('singletap', function() {
-	var id = $(this).data('id');
-	viewNote(id);
-});
+// $('#home-note-list').on('singletap', function() {
+// 	var id = $(this).data('id');
+// 	viewNote(id);
+// });
 
-var notes = [
-	{
-		title : "SimpleNote Intro",
-		content : "Simple haha"
+// var notes = [
+// 	{
+// 		title : "SimpleNote Intro",
+// 		content : "Simple haha"
 	
-	}
-];
+// 	}
+// ];
 
-for (var i = 0; i < notes.length; i ++) {
-	listNode(i);
-}
+// for (var i = 0; i < notes.length; i ++) {
+// 	listNode(i);
+// }
 
-function listNode(id) {
-	$("home-note-list").append("<li class='nav' data-id='" + id + "'>" +
-							   "<h3>" + notes[id].title + "</h3>" + 
-							   "<h4>" + notes[id].content.substr(0, 30) + "...</h4>" +
-							   "</li>"
-							  );
-}
+// function listNode(id) {
+// 	$("home-note-list").append("<li class='nav' data-id='" + id + "'>" +
+// 							   "<h3>" + notes[id].title + "</h3>" + 
+// 							   "<h4>" + notes[id].content.substr(0, 30) + "...</h4>" +
+// 							   "</li>"
+// 							  );
+// }
 
-function viewNote(id) {
-	$('#note-view').data('id', id);
-	$('#note-view .note-title').text(note[id].title);
-	$('#note-view .note-content').html(notes[id].content);
-	$.UIGoToArticle('#note-view');
-}
+// function viewNote(id) {
+// 	$('#note-view').data('id', id);
+// 	$('#note-view .note-title').text(note[id].title);
+// 	$('#note-view .note-content').html(notes[id].content);
+// 	$.UIGoToArticle('#note-view');
+// }
